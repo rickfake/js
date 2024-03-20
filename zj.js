1dfa8900cd457b5a8c0fd824fe343a6270ebeb11
rickfake <89932406+rickfake@users.noreply.github.com>
2024-03-20 15:28:33 +0800

    Update zj.js

--- zj.js
+++ zj.js
@@ -1,23 +1,47 @@
-
 /******************************
 
+oooo
 *******************************
 
 
 
 [rewrite_local]
+
 ^https?://api.revenuecat.com/.+/(receipts$|subscribers/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/rickfake/js/main/qj.js
 
+^https?://api.revenuecat.com/.+/(receipts$|subscribers/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/rickfake/js/main/qj.js
 
-[mitm] 
+[mitm] 
 
-hostname = integral2.zhijiantime.com
+hostname = api.revenuecat.com
 
 *******************************/
 
-const rick = JSON.parse(typeof $response != "undefined" && $response.body || null);
+const py996 = {};
+const py997 = JSON.parse(typeof $response != "undefined" && $response.body || null);
 
-rick.data.ActiveDays=44
-rick.data.VIP=1
+const name = "pro";
+const appid = "app.structured.pro.yearly";
 
-$done(rick);
+  
+if (typeof $response == "undefined") {
+  delete $request.headers["x-revenuecat-etag"];
+  delete $request.headers["X-RevenueCat-ETag"];
+
+  py996.headers = $request.headers;
+} else if (py997 && py997.subscriber) {
+  data = {
+    "expires_date": "9999-09-09T09:09:09Z",
+    "original_purchase_date": "2023-02-23T02:33:33Z",
+    "purchase_date": "2023-02-23T02:33:33Z",
+    "ownership_type": "PURCHASED",
+    "store": "app_store"
+  };
+
+  py997.subscriber.subscriptions[(appid)] = data
+  py997.subscriber.entitlements[(name)] = JSON.parse(JSON.stringify(data));
+  py997.subscriber.entitlements[(name)].product_identifier = (appid);
+  py996.body = JSON.stringify(py997);
+}
+
+$done(py996);
