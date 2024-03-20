d035818e503d4d61a46abe27b421b93773fb02aa
rickfake <89932406+rickfake@users.noreply.github.com>
2024-03-20 16:34:51 +0800

    Update xiaoshuimian.js

--- xiaoshuimian.js
+++ xiaoshuimian.js
@@ -16,7 +16,6 @@ hostname = api.psy-1.com
 *******************************/
 
 var body = $response.body.replace(/is_vip":\d/g,'is_vip":1');
-var body = $response.body.replace(/is_vip":\d/g,'is_vip":1');
 var body = $response.body.replace(/vip_expires":\d/g,'vip_expires":2');
 
 $done({ body });
