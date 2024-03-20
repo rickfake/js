50741d1d0103c4b39ca41ab886b771ba7b868389
rickfake <89932406+rickfake@users.noreply.github.com>
2024-03-20 15:54:28 +0800

    Update flow.js

--- flow.js
+++ flow.js
@@ -8,9 +8,8 @@
 
 [rewrite_local]
 
-^https?://api.revenuecat.com/.+/(receipts$|subscribers/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/rickfake/rick/main/1.js
-
-^https?://api.revenuecat.com/.+/(receipts$|subscribers/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/rickfake/rick/main/1.js
+^https?://api.revenuecat.com/.+/(receipts$|subscribers/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/rickfake/js/main/flow.js
+^https?://api.revenuecat.com/.+/(receipts$|subscribers/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/rickfake/js/main/flow.js
 
 [mitm] 
 
