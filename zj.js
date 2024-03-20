cbf650b18b7831a70e4f52bd25edfc32b8566236
rickfake <89932406+rickfake@users.noreply.github.com>
2024-03-20 15:29:02 +0800

    Update zj.js

--- zj.js
+++ zj.js
@@ -7,9 +7,7 @@ oooo
 
 [rewrite_local]
 
-^https?://api.revenuecat.com/.+/(receipts$|subscribers/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/rickfake/js/main/qj.js
-
-^https?://api.revenuecat.com/.+/(receipts$|subscribers/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/rickfake/js/main/qj.js
+^https?://integral2.zhijiantime.com/v2/User*  url script-response-body https://raw.githubusercontent.com/rickfake/js/main/zj.js
 
 [mitm] 
 
