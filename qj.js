aa2bc998ae94ca39d3bd1187d6b00803545b5e01
rickfake <89932406+rickfake@users.noreply.github.com>
2024-03-20 14:51:31 +0800

    Update qj.js

--- qj.js
+++ qj.js
@@ -8,9 +8,9 @@
 
 [rewrite_local]
 
-^https?://api.revenuecat.com/.+/(receipts$|subscribers/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/rickfake/rick/main/1.js
+^https?://api.revenuecat.com/.+/(receipts$|subscribers/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/rickfake/js/main/qj.js
 
-^https?://api.revenuecat.com/.+/(receipts$|subscribers/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/rickfake/rick/main/1.js
+^https?://api.revenuecat.com/.+/(receipts$|subscribers/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/rickfake/js/main/qj.js
 
 [mitm] 
 
