832dc26c6c1b74ec8a50e2f3ac0bb4bc49d63e86
rickfake <89932406+rickfake@users.noreply.github.com>
2024-03-20 15:15:29 +0800

    Update zj.js

--- zj.js
+++ zj.js
@@ -1,14 +1,12 @@
 
 /******************************
 
-https://integral2.zhijiantime.com/v2/User?ts=1710918016883&sign=238133fbd1fa37bc60287d8c3f37a35d&clientId=1392166974&lang=zh&v=6.4.73&
 *******************************
 
 
 
 [rewrite_local]
-
-^https?://integral2.zhijiantime.com/v2/User* url script-response-body https://raw.githubusercontent.com/rickfake/js/main/zj.js
+^https:\/\/integral2\.zhijiantime.com\/vip\/User*$  url script-response-body https://raw.githubusercontent.com/rickfake/js/main/zj.js
 
 [mitm] 
 
