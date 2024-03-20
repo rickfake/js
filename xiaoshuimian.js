bb4bac768f82760871f48a28a7f77e89737ec3e6
rickfake <89932406+rickfake@users.noreply.github.com>
2024-03-20 16:22:55 +0800

    Create xiaoshuimian.js

---
+++ xiaoshuimian.js
@@ -0,0 +1,22 @@
+/******************************
+
+oooo
+*******************************
+
+
+
+[rewrite_local]
+
+^https?://api.psy-1.com/cosleep/user/info*  url script-response-body https://raw.githubusercontent.com/rickfake/js/main/xiaoshuimian.js
+
+[mitm] 
+
+hostname = api.psy-1.com
+
+*******************************/
+
+var body = $response.body.replace(/is_vip":\d/g,'is_vip":1');
+var body = $response.body.replace(/is_vip":\d/g,'is_vip":1');
+var body = $response.body.replace(/vip_expires":\d/g,'vip_expires":2');
+
+$done({ body });
